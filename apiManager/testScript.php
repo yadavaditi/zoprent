30d5dd16a83302b1b6370e3150af7510ec7e41e9
lalith <lalithl1993@gmail.com>
2018-06-24 22:07:50 +0530

    tested apiManager with new test script

--- apiManager/testScript.php
+++ apiManager/testScript.php
@@ -1,20 +1,20 @@
 <?php
 
-require"onnbikes/class.onn.php";
+require"apiManager.php";
 
- $onn=new class_onn;
+$onn = getAPI('onn');
 
  $parameters = array (
-  'fromDate' => '2018-06-24 09:00:00 +0530',
-  'toDate' => '2018-06-25 09:00:00 +0530',
+  'fromDate' => '2018-06-28 09:00:00 +0530',
+  'toDate' => '2018-06-29 09:00:00 +0530',
   'cityLatitude' => 0,
   'cityLongitude' => 0,
   'cityName' => 'Bengaluru',
 );
  $var=$onn->getAvailableBikes($parameters);
  echo"... <br>";
- print_r($var['bikeStations'][0]['bikes'][0]['bikeId']);
- //echo json_encode($var);
+ //print_r($var['bikeStations'][0]['bikes'][0]['bikeId']);
+ echo json_encode($var);
 
 
 ?>
